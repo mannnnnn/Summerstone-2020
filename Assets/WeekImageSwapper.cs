036794708a2396f45c6467bd4351f26f4bbfd585
Savannah Mann <mannnnnn@users.noreply.github.com>
2020-03-01 15:37:37 -0600

    Week/Weekend Image Swapper added
    Now to put some nicer images in there...

---
+++ Assets/WeekImageSwapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeekImageSwapper : MonoBehaviour
+{
+    public Image currImage;
+
+    public Sprite[] weekImages = new Sprite[10];
+    public Sprite[] weekendImages = new Sprite[10];
+
+    public void updateImage(bool isWeek, int index)
+    {
+        if (isWeek)
+        {
+            if (currImage != null && weekImages[index] != null)
+            {
+                currImage.sprite = weekImages[index];
+            }
+        } else
+        {
+            if (currImage != null && weekendImages[index] != null)
+            {
+                currImage.sprite = weekendImages[index];
+            }
+        }
+        
+    }
+}
