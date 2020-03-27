73e1c40e9e21db2d3dcff256f9cae27700a4f0fe
Richitt <richitt10@gmail.com>
2020-03-26 22:09:42 -0400

    finalizing

--- Assets/Scripts/DisplayButton.cs
+++ Assets/Scripts/DisplayButton.cs
@@ -35,40 +35,11 @@ public class DisplayButton : MonoBehaviour
       SetResultColor(c);
     }
     public void displayWhite(){
-      MastermindColor c = mastermind.displayColors[0];
+      MastermindColor c = mastermind.displayColors[1];
       SetResultColor(c);
     }
     public void displayNull(){
-      MastermindColor c = mastermind.displayColors[0];
-      SetResultColor(c);
-    }
-
-
-    public void displaySuccess(){
-      MastermindColor c = mastermind.displayColors[0];
-      SetResultColor(c);
-      Debug.Log("everything is correct");
-    }
-
-    public void displayFailure(){
-      MastermindColor c = mastermind.displayColors[1];
-      SetResultColor(c);
-      Debug.Log("ran out of tries");
-    }
-
-    public void displayCorrectColor(){
-      MastermindColor c = mastermind.displayColors[3];
-      SetResultColor(c);
-      Debug.Log("you have a correct color");
-    }
-    public void displayCorrectColorSpace(){
-      MastermindColor c = mastermind.displayColors[4];
-      SetResultColor(c);
-      Debug.Log("at least one peg in correct location and color");
-    }
-    public void displayIncorrect(){
       MastermindColor c = mastermind.displayColors[2];
       SetResultColor(c);
-      Debug.Log("void");
     }
 }
